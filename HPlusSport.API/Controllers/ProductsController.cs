970aeb7319c50d7a35eeea6be1d63b6b840b874d
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2026-02-08 21:30:54 +0530

    Add GetProducts action method

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace HPlusSport.API.Controllers
 {
@@ -7,5 +6,10 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "OK";
+        }
     }
 }
